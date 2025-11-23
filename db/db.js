470c7f09367f3abeb449dac3db8e623f0e1945c8
Savio dos Santos <savio.dossantos@outlook.com>
2025-11-23 00:42:51 -0300

    hc

--- db/db.js
+++ db/db.js
@@ -22,8 +22,8 @@ async function conecta() {
     const mysql = require("mysql2/promise")
     const conn = await mysql.createConnection({
         host: "localhost",
-        user: "svtest",
-        password: "123456",
+        user: "root",
+        password: "",
         database: "projeto_videos_new"
     })
     globalThis.connection = conn
